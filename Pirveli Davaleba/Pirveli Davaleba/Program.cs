a35d5c80538ca20fcd7eec5b317a7a94c77044e3
Baxtura1998 <76566697+Baxtura1998@users.noreply.github.com>
2020-12-24 21:04:17 +0400

    Add files via upload
    Pirveli Davaleba

---
+++ Pirveli Davaleba/Pirveli Davaleba/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Pirveli_Davaleba
+{
+    class Program
+    {
+        //Funqcia
+        public static bool isPalindrome(string text)
+        {
+            for(int i=0;i<text.Length;i++)
+            {
+                if (text[i] != text[text.Length - 1 - i])
+                {
+                    Console.WriteLine("Ar Aris Palindromi");
+                    return false;
+                }                                
+            }
+            Console.WriteLine("Aris Palindromi");
+            return true;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Shemoitanet Teqsti Da GavigoT, Aris Tu Ara Palindromi");
+            string text = Console.ReadLine(); //Shemogvaq Teqsti
+            if(true)
+                Console.WriteLine(isPalindrome(text)); //Vigebt Palindromia Tu Ara
+        }
+    }
+}
